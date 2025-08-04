b613e32e3e2026ff3db901fdd0fb292ad98c98ab
Aulya <aulyathareeq@gmail.com>
2025-08-04 15:19:35 +0700

    Updated styling

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,13 +3,22 @@ import "./About.css";
 export function About() {
   return (
     <div className="about-section">
-      <p className="about-desc">Building interfaces, game systems, and ideas worth exploring.</p>
+      <p className="about-desc">
+        Building interfaces,{" "}
+        <a href="https://a.itch.io/">
+          <span>
+            game<sup className="sup">↗</sup>
+          </span>
+        </a>{" "}
+        systems, and ideas worth exploring.
+      </p>
 
       <div className="about-links">
-        <a href="mailto:aulyathareeq2@gmail.com">Get in Touch ↗</a>
-      </div>
-      <div className="about-links">
-        <a href="https://ciderbuild.itch.io/">Itch.io Page</a>
+        <a href="mailto:a@gmail.com">
+          <span>
+            Get in Touch<sup className="sup">↗</sup>
+          </span>
+        </a>
       </div>
     </div>
   );
